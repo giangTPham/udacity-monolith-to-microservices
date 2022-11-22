babb16e0991346634880ecd3786cbcd4cc76a1a4
GiangTPham <phatr2000@gmail.com>
2022-11-22 20:56:32 +0700

    Deploy to Cloud

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a27f85d912aa746c094851ce8b29a89a-2049577633.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
